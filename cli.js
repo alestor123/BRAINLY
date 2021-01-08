6f0706aa4eb2a8df966e8716b7d00d83c48c2c5f
alestor123 <alestoraldous@gmail.com>
2021-01-08 19:14:00 +0530

    added main code

--- cli.js
+++ cli.js
@@ -1,1 +1,15 @@
 #!/usr/bin/env node
+
+var brain = require('./App'),
+chalk = require('chalk'),
+figlet = require('figlet');
+
+(async () => {
+var data = await brain();
+figlet('Todays Quote',(err, tdata) => {
+if(err) throw err
+console.log(chalk.yellow(tdata))
+console.log('\n Name : ' +chalk.yellow(data.name) +  ' \n' + '\n Quote : ' + chalk.green(data.quote) + '\n' )
+
+});
+})();
